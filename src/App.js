58ef22b694ad8e2ac4f0e2143dcde2c30b7f44ff
ntqams30 <msievers@thegeneral.com>
2022-02-21 16:45:30 -0600

    Part 3 update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import React from "react";
 import Card from "./Card";
+import './App.css'
 
-const App = () => {
-    return(
-        <div>
-            <h1>Music I Like</h1>
-            <Card
-                albumTitle="10,000 Days"
-                albumDescription="Some album stuff would usually go here to descript the album."
-                imgUrl="https://picsum.photos/100/100"
-                buttonText="OK"
-            />
-            <Card/>
-            <Card/>
-        </div>
-    )
+class App extends React.Component {
+
+    state = {albumList : [
+            {
+                "id": 0,
+                "title": "10,000 Days",
+                "description": "Some album stuff would usually go here to description the album.",
+                "year": 2006,
+                "imgUrl": "https://picsum.photos/100/100",
+                "buttonText": "OK"
+            },
+            {
+                "id": 1,
+                "title": "Aenima",
+                "description": "Only the greatest tool album ever.",
+                "year": 1996,
+                "imgUrl": "https://picsum.photos/100/100",
+                "buttonText": "OK"
+            },
+            {
+                "id": 2,
+                "title": "Fear Inoculum",
+                "description": "The newest album.",
+                "year": 2019,
+                "imgUrl": "https://picsum.photos/100/100",
+                "buttonText": "OK"
+            }
+        ]}
+
+    renderList = () => {
+        return this.state.albumList.map(
+            (album) => {
+                return (<Card albumTitle={album.title} albumDescription={album.description} buttonText={album.buttonText} imgUrl={album.imgUrl}/>)
+            }
+        )
+    }
+
+    render() {
+        return (
+            <div className="container">
+                {this.renderList()}
+            </div>
+        )
+    }
 }
+
+export default App
